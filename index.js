783fd1918d20c63ac02b4e99b2deca709222e704
Trawwis <speckichan5@gmail.com>
2025-03-15 11:12:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["EDIT THE STATUS", "🎮 Playing VALORANT"];
+const statusMessages = ["Goty Bot by 14noah", "🎮 Playing MIGHTY SELLING"];
 let currentIndex = 0;
 
 async function login() {
