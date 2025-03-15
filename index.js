f689b2fa787509415ec03293ae0886d6eed23f91
Trawwis <speckichan5@gmail.com>
2025-03-15 11:22:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["EDIT THE STATUS", "🎮 Playing VALORANT"];
+const statusMessages = ["Listening to Mighty Selling", "🎮 Playing Mighty Selling"];
 let currentIndex = 0;
 
 async function login() {
